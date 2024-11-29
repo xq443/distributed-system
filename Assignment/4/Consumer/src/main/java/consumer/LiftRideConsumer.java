a06e61b3cd1ffdab2590fd31c4182b7e3f40e119
Cathy Qin <qin.xuj@northeastern.edu>
2024-11-29 01:54:43 -0800

    modify redis key

--- Assignment/4/Consumer/src/main/java/consumer/LiftRideConsumer.java
+++ Assignment/4/Consumer/src/main/java/consumer/LiftRideConsumer.java
@@ -59,20 +59,26 @@ public class LiftRideConsumer {
 
           // Process the message and store it in Redis
           try (Jedis jedis = jedisPool.getResource()) {
+            // Calculate vertical from liftID
+            int liftVertical = liftRide.getLiftID() * 10; // Vertical = liftID * 10
+
+            // GET/resorts/{resortID}/seasons/{seasonID}/day/{dayID}/skiers
             String redisKey1 = "resort:" + liftRide.getResortID() +
                 ":season:" + liftRide.getSeasonID() +
                 ":day:" + liftRide.getDayID();
             jedis.sadd(redisKey1, String.valueOf(liftRide.getSkierID()));
 
-//            long uniqueSkierCount = jedis.scard(redisKey1);
-//            System.out.println("Unique skier count for " + redisKey + ": " + uniqueSkierCount);
-
-            String redisKey2 = "resort:" + liftRide.getResortID() +
+            // GET/skiers/{resortID}/seasons/{seasonID}/days/{dayID}/skiers/{skierID}
+            String dailyKey = "resort:" + liftRide.getResortID() +
                 ":season:" + liftRide.getSeasonID() +
                 ":day:" + liftRide.getDayID() +
                 ":skier:" + liftRide.getSkierID();
+            jedis.lpush(dailyKey, String.valueOf(liftRide.getLiftID()));
 
-            jedis.lpush(redisKey2, String.valueOf(liftRide.getLiftID()));
+            // GET/skiers/{skierID}/vertical
+            String verticalKey = "skier:" + liftRide.getSkierID() + ":vertical";
+            String seasonField = liftRide.getResortID() + ":" + liftRide.getSeasonID(); // grouped by resort and season
+            jedis.hincrBy(verticalKey, seasonField, liftVertical);
           } catch (Exception e) {
             System.err.println("Error processing message for skierID " + liftRide.getSkierID() + ": " + e.getMessage());
           }
